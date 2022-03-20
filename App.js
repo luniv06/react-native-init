f3953f1d22141ff4e78a76a7d120c73f874993bc
Vinul James <vinuljames@gmail.com>
2022-03-20 16:06:23 +0530

    Button Click Counter

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -7,12 +7,37 @@ import {
   Text,
   useColorScheme,
   View,
+  Button,
+  Linking,
 } from 'react-native';
 
 const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  const onButtonClick = () => {
+    // Linking.openURL('https://www.twitch.tv/valorantesports3');
+    setCounter(counter + 1);
+  };
+
+  const onButtonReset = () => {
+    // Linking.openURL('https://www.twitch.tv/valorantesports3');
+    setCounter(0);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
+      <Text style={styles.text}>Button Click Counter</Text>
+      <Text style={styles.counter}> {counter} </Text>
+      <Button title="Update Count" onPress={onButtonClick}>
+        {'  '}
+      </Button>
+      <Text
+        style={{color: 'white', fontSize: 15, marginTop: 40, marginBottom: 20}}>
+        You clicked the button {counter} times
+      </Text>
+      <Button title="Reset Count" onPress={onButtonReset}>
+        {'  '}
+      </Button>
     </View>
   );
 };
@@ -23,6 +48,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#320f06',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: 'white',
+    fontSize: 15,
+    margin: 5,
+  },
+  counter: {
+    color: 'lightblue',
+    fontSize: 25,
+    margin: 10,
+    fontStyle: 'italic',
   },
 });
 
